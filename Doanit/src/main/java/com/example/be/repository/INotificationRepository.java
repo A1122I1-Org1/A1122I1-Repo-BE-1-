89c8598839393cb6a0986fefd97662066da25be0
vankhang3006 <119319324+vankhang3006@users.noreply.github.com>
2023-12-12 21:29:17 +0700

    lan dau

--- Doanit/src/main/java/com/example/be/repository/INotificationRepository.java
+++ Doanit/src/main/java/com/example/be/repository/INotificationRepository.java
@@ -18,7 +18,7 @@ public interface INotificationRepository extends JpaRepository<Notification, Int
     Page<Notification> findNotificationsByAccountId(Integer accountId, Pageable pageable);
 
 
-    @Query(value = "SELECT notification_id, content, status, time_notification, title, url, account_id, account_send_notification_id FROM doanit.notification WHERE notification.account_id = ?1  AND notification.status = 0  ORDER BY notification.time_notification DESC", nativeQuery = true)
-    List<Notification> findByAccountIdAndStatusIsTrue(Integer accountId);
+    @Query(value = "SELECT notification_id, content, status, time_notification, title, url, account_id, account_send_notification_id FROM doanit.notification WHERE notification.account_id = ?1  AND notification.notification_id = ?2 AND notification.status = 0  ORDER BY notification.time_notification DESC", nativeQuery = true)
+    List<Notification> findByAccountIdAndStatusIsTrue(Integer accountId, Integer notificationId);
 
 }
